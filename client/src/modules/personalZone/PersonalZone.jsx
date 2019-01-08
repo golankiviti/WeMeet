34627106e4237e0b5962f35949242e62c0a535ab
Unknown <golkiv@gmail.com>
2019-01-08 19:47:54 +0200

    /locations_client:
    merge

--- client/src/modules/personalZone/PersonalZone.jsx
+++ client/src/modules/personalZone/PersonalZone.jsx
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import LocationListContainer from './locations/LocationListContainer';
+import MyMeetingsContainer from './meetings/myMeetings/MyMeetingsContainer';
 import styles from './personalZone.module.scss';
 
 class PersonalZone extends Component {
     render() {
         return <div className={styles.container}>
             <LocationListContainer />
+            <MyMeetingsContainer />
         </div>
     }
 }
