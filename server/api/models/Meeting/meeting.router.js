a7f3ab912a69298deada8ca2c2437c7212593b0a
dinzone <dinamitdana1@gmail.com>
2019-04-05 19:41:33 +0300

    add GET waiting meetings for user + the option to accept or reject the meeting

--- server/api/models/Meeting/meeting.router.js
+++ server/api/models/Meeting/meeting.router.js
@@ -7,4 +7,15 @@ const router = express.Router();
 router.get('/:id', meetingController.getUserMeetings);
 router.put('/', meetingController.creatNewMeeting);
 router.post('/', meetingController.updateMeeting);
+
+/**
+ * users waiting meetings
+ */
+
+// get all user waitingMeetings
+// :id should be userId and not meetingId
+router.get('/waitingMeetings/:id', meetingController.getUserWaitingMeetings);
+// accept of reject meeting by user
+router.post('/meetingResponse', meetingController.acceptOrRejectMeeting);
+
 module.exports = router;
--- server/api/models/Meeting/meeting.service.js
+++ server/api/models/Meeting/meeting.service.js
@@ -1,23 +1,149 @@
-const mongoose = require('mongoose');
+const mongoose = require('mongoose'),
+    ObjectId = mongoose.Types.ObjectId,
+    moment = require('moment'),
+    _ = require('lodash');
 
 const meeting = require('../../../data').schemas.meetings;
 
+const logger = require('../../../utils/logger');
+
 const getUserMeetings = (userId) => {
-    return  meeting.find({ $or: [ {"creator" : userId}, {"participants" : {$in:  userId}}] });
+    return meeting.find({
+        $or: [{
+            "creator": userId
+        }, {
+            "participants": {
+                $in: userId
+            }
+        }]
+    });
 };
 
 const creatNewMeeting = (currentMeeting) => {
 
-   let newMeeting = new meeting(currentMeeting);
+    let newMeeting = new meeting(currentMeeting);
     return newMeeting.save();
 };
 const updateMeeting = (currentMeeting) => {
-    return meeting.findOneAndUpdate({ '_id': new mongoose.Types.ObjectId(currentMeeting._id) }, currentMeeting);
- };
+    return meeting.findOneAndUpdate({
+        '_id': new mongoose.Types.ObjectId(currentMeeting._id)
+    }, currentMeeting);
+};
+
+const getUserWaitingMeetings = (userId) => {
+    const query = {
+        $and: [{
+                // not relevat if the meeting already started
+                fromDate: {
+                    $gt: moment().toDate()
+                }
+            },
+            {
+                // the user need to be creator or one of the participants
+                $or: [{
+                    creator: userId,
+                }, {
+                    participants: userId
+                }],
+            },
+            {
+                // the user should not be in the accepted list
+                accepted: {
+                    $ne: userId
+                }
+            },
+            {
+                // the user should not be in the rejected list
+                rejected: {
+                    $ne: userId
+                }
+            }
+        ]
+    };
+    return meeting.find(query).lean();
+};
+
+const acceptMeeting = (userId, meetingId) => {
+    logger.debug(`find meeting with id ${meetingId}`);
+    return meeting.findById(meetingId).lean()
+        .then((meet) => {
+            return Promise.resolve()
+                .then(() => {
+                    // in case the user exist in the rejected list
+                    logger.debug('check if the user exist in the rejected list');
+                    if (_.findIndex(meet.rejected, new ObjectId(userId)) !== -1) {
+                        logger.debug('remove user from rejected list');
+                        return meeting.updateOne({
+                            _id: meetingId
+                        }, {
+                            $pull: {
+                                rejected: userId
+                            }
+                        });
+                    }
+                    return Promise.resolve();
+                })
+                .then(() => {
+                    // in case the user not exist in the accepted list
+                    logger.debug('check if the user already exist in the accepted list');
+                    if (_.findIndex(meet.accepted, new ObjectId(userId)) !== -1) {
+                        return Promise.resolve();
+                    }
+                    logger.debug('put the user in the accepted list');
+                    return meeting.updateOne({
+                        _id: meetingId
+                    }, {
+                        $push: {
+                            accepted: userId
+                        }
+                    });
+                });
+        });
+}
+
+const rejectMeeting = (userId, meetingId) => {
+    logger.debug(`find meeting with id ${meetingId}`);
+    return meeting.findById(meetingId).lean()
+        .then((meet) => {
+            return Promise.resolve()
+                .then(() => {
+                    // in case the user exist in the accepted list
+                    logger.debug('check if the user exist in the accepted list');
+                    if (_.findIndex(meet.accepted, new ObjectId(userId)) !== -1) {
+                        logger.debug('remove user from accepted list');
+                        return meeting.updateOne({
+                            _id: meetingId
+                        }, {
+                            $pull: {
+                                accepted: userId
+                            }
+                        });
+                    }
+                    return Promise.resolve();
+                })
+                .then(() => {
+                    // in case the user not exist in the rejected list
+                    logger.debug('check if the user already exist in the rejected list');
+                    if (_.findIndex(meet.rejected, new ObjectId(userId)) !== -1) {
+                        return Promise.resolve();
+                    }
+                    logger.debug('put the user in the accepted list');
+                    return meeting.updateOne({
+                        _id: meetingId
+                    }, {
+                        $push: {
+                            rejected: userId
+                        }
+                    });
+                });
+        });
+}
 
 module.exports = {
     getUserMeetings,
     creatNewMeeting,
-    updateMeeting
-
+    updateMeeting,
+    getUserWaitingMeetings,
+    acceptMeeting,
+    rejectMeeting
 };
