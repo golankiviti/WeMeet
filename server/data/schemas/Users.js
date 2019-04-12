c0f076cfc31f522b9d00b37d5a0af508fc1e59ea
dinzone <dinamitdana1@gmail.com>
2019-04-12 12:16:10 +0300

    tmp

--- server/data/schemas/Users.js
+++ server/data/schemas/Users.js
@@ -1,7 +1,9 @@
 // define the users schema
 
 const mongoose = require('mongoose'),
-    bcrypt = require('bcrypt-nodejs');
+    bcrypt = require('bcrypt-nodejs'),
+    jwt = require('jsonwebtoken'),
+    moment = require('moment');
 
 let schema = mongoose.Schema({
     local: {
@@ -28,5 +30,17 @@ schema.methods.generateHash = function (password) {
 schema.methods.validPassword = function (password) {
     return bcrypt.compareSync(password, this.local.password);
 };
+
+schema.methods.generateJWT = function () {
+    const today = moment().toDate();
+    const expirationDate = moment(today);
+    expirationDate = expirationDate.add(60, 'd').toDate();
+
+    return jwt.sign({
+        email: this.email,
+        id: this._id,
+        exp: parseInt(expirationDate.getTime() / 1000, 10),
+    }, 'secret');
+}
 
 module.exports = mongoose.model('user', schema);
--- server/index.js
+++ server/index.js
@@ -78,7 +78,7 @@ app.post('/checkUser', authenticationService.isUserExist);
 
 
 // export /api route
-app.use('/api', apiRouter);
+app.use('/api', authenticationService.isLoggedIn, apiRouter);
 
 // export production client side
 app.use(express.static(path.join(__dirname, '/build')));
