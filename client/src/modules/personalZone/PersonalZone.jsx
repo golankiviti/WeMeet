e50d796b8d79ba87a2a6b0b26e4cd47bc7615007
Unknown <golkiv@gmail.com>
2019-05-27 21:06:43 +0300

    redesign of personal zone

--- client/src/modules/personalZone/PersonalZone.jsx
+++ client/src/modules/personalZone/PersonalZone.jsx
@@ -1,23 +1,58 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
+import { List } from 'immutable';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types'
 import LocationListContainer from './locations/LocationListContainer';
 import MyMeetingsContainer from './meetings/myMeetings/MyMeetingsContainer';
 import MeetingsForApprovalContainer from './meetingsForApproval/MeetingsForApprovalContainer';
 import FutureMeetingsContainer from './futureMeetings/FutureMeetingsContainer';
 import styles from './personalZone.module.scss';
+import { getMeetingsForApproval, getMeetings, getFutureMeetings } from '../../clientManager/meetingsClientManager';
+import { userLocations } from '../../clientManager/userManager';
+import classNames from 'classnames';
 
-class PersonalZone extends Component {
-    render() {
-        return <div className={styles.container}>
-            <div className={styles.sideContainer}>
-                <MeetingsForApprovalContainer />
-                <LocationListContainer />
-            </div>
-            <div className={styles.sideContainer}>
-                <MyMeetingsContainer />
-                <FutureMeetingsContainer />
-            </div>
+const propTypes = {
+    userId: PropTypes.string
+}
+function PersonalZone({ userId }) {
+    const [meetingsForApproval, setMeetingsForApproval] = useState(List())
+    const [locations, setLocations] = useState(List())
+    const [myMeetings, setMyMeetings] = useState(List())
+    const [futureMeetings, setFutureMeetings] = useState(List())
+
+    useEffect(() => {
+        Promise.all([
+            getMeetingsForApproval(userId),
+            getMeetings(userId),
+            userLocations(userId),
+            getFutureMeetings(userId)
+        ]).then(res => {
+            setMeetingsForApproval(List(res[0]))
+            setLocations(List(res[1]))
+            setMyMeetings(List(res[2]))
+            setFutureMeetings(List(res[3]))
+        })
+    }, [])
+
+    const rightContainerClasses = classNames(styles.innerContainer, styles.right);
+    const leftContainerClasses = classNames(styles.innerContainer, styles.left);
+
+    return <div className={styles.container}>
+        <div className={rightContainerClasses}>
+            <MeetingsForApprovalContainer />
+            <FutureMeetingsContainer />
         </div>
-    }
+        <div className={leftContainerClasses}>
+            <MyMeetingsContainer />
+            <LocationListContainer />
+        </div>
+    </div>
 }
 
-export default PersonalZone; 
+PersonalZone.propTypes = propTypes;
+
+const mapStateToProps = state => ({
+    userId: state.user ? state.user.get('_id') : ''
+});
+
+export default connect(mapStateToProps)(PersonalZone);
--- client/src/modules/personalZone/locations/LocationList.jsx
+++ client/src/modules/personalZone/locations/LocationList.jsx
@@ -10,6 +10,8 @@ import EditLocation from './locationDialogs/EditLocation';
 import styles from './locationList.module.scss';
 import CircularProgress from '@material-ui/core/CircularProgress';
 import classNames from 'classnames';
+import Fab from '@material-ui/core/Fab';
+import AddIcon from '@material-ui/icons/Add';
 
 const propTypes = {
     locations: ImmutablePropTypes.list.isRequired,
@@ -77,9 +79,9 @@ class LocationList extends Component {
                     )
                 }
             </div>
-            <Button className={styles.addButton} onClick={this.handleOpenAddDialog}>
-                <Icon>add</Icon>
-            </Button>
+            <Fab className={styles.addButton} color='primary' aria-label='Add' onClick={this.handleOpenAddDialog}>
+                <AddIcon />
+            </Fab>
             {
                 this.state.addDialogOpen &&
                 <AddLocation userId={userId}
